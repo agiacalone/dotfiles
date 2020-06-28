d6671c964cda0bae2ce56bd8c653f898a31f4d20
Anthony Giacalone <agiacalone@gmail.com>
2020-06-28 13:46:33 -0700

    changed mac-brew to be more fully-featured for a new install of

--- mac-brew-gnu.sh
+++ mac-brew-gnu.sh
@@ -1,4 +1,14 @@
-# see https://www.topbug.net/blog/2013/04/14/install-and-use-gnu-command-line-tools-in-mac-os-x/
+# Installer for new Mac installs
+# This script will install homebrew, oh-my-zsh, and others
+
+
+# First, install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# Install oh-my-zsh
+brew install curl
+brew install zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # core
 brew install coreutils
@@ -6,19 +16,19 @@ brew install coreutils
 # key commands
 brew install binutils
 brew install diffutils
-brew install ed --default-names
-brew install findutils --with-default-names
+brew install ed
+brew install findutils
 brew install gawk
-brew install gnu-indent --with-default-names
-brew install gnu-sed --with-default-names
-brew install gnu-tar --with-default-names
-brew install gnu-which --with-default-names
+brew install gnu-indent
+brew install gnu-sed
+brew install gnu-tar
+brew install gnu-which
 brew install gnutls
-brew install grep --with-default-names
+brew install grep
 brew install gzip
 brew install screen
 brew install watch
-brew install wdiff --with-gettext
+brew install wdiff
 brew install wget
 
 # OS X ships a GNU version, but too old
@@ -35,11 +45,49 @@ brew install file-formula
 brew install git
 brew install less
 brew install openssh
-brew install perl518   # must run "brew tap homebrew/versions" first!
 brew install python
 brew install rsync
 brew install svn
 brew install unzip
+brew install unrar
 brew install vim --override-system-vi
 brew install macvim --override-system-vim --custom-system-icons
-brew install zsh
+brew install htop
+brew install newsboat
+brew install neomutt
+brew install slrn
+brew install weechat
+brew install nethack
+brew install moreutils
+brew install ddate
+brew install frotz
+brew install inform6
+brew install mosh
+brew install swi-prolog
+brew install lftp
+brew install netcat
+brew install bison
+brew install flex
+
+# Special for BSD Games
+brew install bsdmake
+brew tap ctdk/ctdk
+brew install bsdgames-osx
+
+# Generate the SSH key
+ssh-keygen
+
+# Display the public key for copying
+cat ~/.ssh/id_rsa.pub
+read -p "Copy the public key to Github and then press [Enter] to continue..."
+# Make the directories and copy stuff from Github
+mkdir ~/bin
+mkdir ~/git
+mkdir ~/git/dotfiles
+git clone git@github.com:agiacalone/dotfiles.git ~/git/dotfiles
+
+# First copy the non-dot files, then the dot files
+cp -R ~/git/dotfiles/* ~/
+cp -R ~/git/dotfiles/.* ~/
+
+# The initial installation setup should be complete!
