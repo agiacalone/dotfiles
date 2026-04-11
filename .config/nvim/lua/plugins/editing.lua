e0beb93dddbe536cd99bc4d2ce48b8d9662e770a
Anthony <anthony@giatek.net>
2026-04-11 13:57:01 -0700

    remove redundant vim-markdown

--- .config/nvim/lua/plugins/editing.lua
+++ .config/nvim/lua/plugins/editing.lua
@@ -6,15 +6,6 @@ return {
   -- Alignment
   { "godlygeek/tabular" },
 
-  -- Markdown syntax
-  {
-    "plasticboy/vim-markdown",
-    config = function()
-      vim.g.vim_markdown_folding_disabled = 1
-      vim.g.vim_markdown_conceal          = 0
-    end,
-  },
-
   -- Markdown rendering in buffer
   {
     "MeanderingProgrammer/render-markdown.nvim",
